91dbbf9325cf569df1636d32f06476aa7161365c
Chase Johnston <46426781+blueberrycola@users.noreply.github.com>
2021-04-08 02:55:26 -0400

    done

--- vis.py
+++ vis.py
@@ -1,8 +1,22 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+
+#attr values
+DATE = 0
+DEPTH = 1
+TEMP = 2
+COND = 3
+DO = 4
+TURB = 5
+CHL = 6
+BATV = 7
 
 #Open txt file and load into buffer
 filename = "lakeerie_data.txt"
-buffer = [] #raw data moved to list
-data = [] #data that goes with Option A of assignment
+buffer = [] #raw data used for chart 1
+data = [] #data that goes with Option A of assignment, used for chart 2
+Ydata = []
+Xdata = []
 
 
 i=0
@@ -13,8 +27,30 @@ with open(filename) as fin:
             myline = line.split()
             buffer.append(myline)
         i+=1
-#Create var for buffer size
-i -= 3
-SIZE = i
-print(buffer[0])
-#Go thru buffer
+#Scroll thru entries, append if do < 2.0 to data 
+for entry in buffer:
+    i=0
+    match = False
+    for item in entry:
+        if(i==DATE):
+            Xdata.append(float(item))
+        if(i == DO):
+            if(float(item) <= 1.99):
+                match = True
+                Ydata.append(float(item))
+        i+=1
+    if(match):
+        print(entry)
+        data.append(entry)
+
+
+#Create visualization from entries in buffer and data ENLARGE MATLIBPLOT for better view!
+plt.style.use('fivethirtyeight')
+plt.title("Amount of entries for when Dissolved O2 < 2 mg/L")
+plt.xlabel("Amount")
+plt.ylabel("O2 Level mg/L")
+bin = [-0.15, -0.10, 0.00, 0.10, 0.25, 0.50, 0.75, 1.00, 1.25, 1.50, 1.99]
+plt.hist(Ydata, bins=bin)
+plt.show()
+
+                
